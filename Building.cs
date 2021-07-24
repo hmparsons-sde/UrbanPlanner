0e1a4f3ac03961aec538f25dc6fdcfad8f37f7f1
Holly Parsons <holly-parsons@outlook.com>
2021-07-24 11:23:32 -0500

    added notes

--- Building.cs
+++ Building.cs
@@ -7,19 +7,21 @@ namespace UrbanPlanner
     class Building
     {
         //Private Fields
+        // Fields typically store the data that must be accessible to more than one class method and must be stored for longer than the lifetime of any single method.
+        // an underscore denotes private fields.
         private string _designer = "Holly Parsons";
         private DateTime _dateConstructed = DateTime.Now;
         private string _address = "123 electric avenue, nashville, tn, 37206";
         private string _owner = "Holly Parsons";
 
-        public int Stories { get; set; } = 2;
+        public int Stories { get; set; } = 2; // REMINDER: A get property accessor is used to return the property value, and a set property accessor is used to assign a new value. 
         public double Width { get; set; } = 50.2;
         public double Height { get; set; } = 65.5;
         public double Volume
         {
-            get => Width * Height * (3 * Stories);
+            get => Width * Height * (3 * Stories); // read-only
         }
-        public Building(string address)
+        public Building(string address) // REMINDER: Constructors set default values & limit instantiation
         {
             _address = address;
         }
@@ -33,7 +35,7 @@ namespace UrbanPlanner
             _owner = newOwner;
             return _owner;
         }
-        public string Designer() => _designer;
+        public string Designer() => _designer; // REMINDER: If a constructor can be implemented as a single statement, you can use an expression body definition
         public string Address() => _address;
         public string Owner() => _owner;
 
--- City.cs
+++ City.cs
@@ -8,13 +8,13 @@ namespace UrbanPlanner
     {
         public string Name { get; set; }
         public string Mayor { get; set; }
-        public List<Building> Buildings { get; set; } = new List<Building>();
+        public List<Building> Buildings { get; set; } = new List<Building>(); // Represents a strongly typed list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists.
 
         private int _yearEstablished = 1779;
 
         public void AddBuilding(Building building)
         {
-            Buildings.Add(building);
+            Buildings.Add(building); // Adds an object to the end of the list.
         }
         public int showYear() => _yearEstablished;
 
